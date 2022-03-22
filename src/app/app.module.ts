41642522099de325a2c1963ab939cbe038b71b0a
Emilio Fernández <barbulina@gmail.com>
2022-03-22 15:49:28 +0100

    register serviworker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RationListComponent } from './shared/components/ration-list/ration-list.component';
-import { RationFormComponent } from './shared/components/ration-form/ration-form.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularMaterialModule } from './shared/modules/angular-material/angular-material.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { CONFIG } from './shared/config';
 import { BloodGlucoseIndexComponent } from './shared/components/blood-glucose-index/blood-glucose-index.component';
+import { RationFormComponent } from './shared/components/ration-form/ration-form.component';
+import { RationListComponent } from './shared/components/ration-list/ration-list.component';
+import { CONFIG } from './shared/config';
+import { AngularMaterialModule } from './shared/modules/angular-material/angular-material.module';
 import { BloodGlucoseIndexColorPipe } from './shared/pipes/blood-glucose-index-color.pipe';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -46,9 +45,7 @@ export function createTranslateLoader(http: HttpClient) {
     }),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerImmediately'
     })
   ],
   providers: [],
