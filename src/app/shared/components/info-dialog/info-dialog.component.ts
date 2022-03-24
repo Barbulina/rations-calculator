99f0efc357c6c2128e5af7fc5f4f4a3292872062
Emilio Fernández <barbulina@gmail.com>
2022-03-24 10:37:16 +0100

    close button

--- src/app/shared/components/info-dialog/info-dialog.component.ts
+++ src/app/shared/components/info-dialog/info-dialog.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-info-dialog',
   templateUrl: './info-dialog.component.html',
   styleUrls: ['./info-dialog.component.scss']
 })
-export class InfoDialogComponent {}
+export class InfoDialogComponent {
+  constructor(public dialogRef: MatDialogRef<InfoDialogComponent>) {}
+  close() {
+    this.dialogRef.close();
+  }
+}
